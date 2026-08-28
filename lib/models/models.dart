085409614ea23e99fe646c2835ea72094cd733e5
rafailalikhan-a11y <rafail.ali.khan@gmail.com>
2026-08-28 17:06:52 +0300

    Update models.dart

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -33,4 +33,21 @@ class Signal {
   final double score;
   Signal({required this.symbol, required this.type, required this.price,
       required this.volRatio, required this.rsi14, required this.reason, required this.score});
+
+  /// Unique identity for "is this a new signal" comparisons.
+  String get key => '$symbol-${type.name}';
+
+  Map<String, dynamic> toJson() => {
+    'symbol': symbol, 'type': type.name, 'price': price, 'volRatio': volRatio,
+    'rsi14': rsi14, 'reason': reason, 'score': score,
+  };
+  factory Signal.fromJson(Map<String, dynamic> j) => Signal(
+    symbol: j['symbol'],
+    type: j['type'] == 'sell' ? SignalType.sell : SignalType.buy,
+    price: (j['price'] as num).toDouble(),
+    volRatio: (j['volRatio'] as num).toDouble(),
+    rsi14: (j['rsi14'] as num).toDouble(),
+    reason: j['reason'] ?? '',
+    score: (j['score'] as num).toDouble(),
+  );
 }
